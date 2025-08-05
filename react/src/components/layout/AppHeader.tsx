4f4a43e17ea7176ac472fb8ab8404c35b9415f1b
Ranjot Dharni <ranjotdharni1@gmail.com>
2025-08-04 22:10:03 -0700

    app header layout complete

--- react/src/components/layout/AppHeader.tsx
+++ react/src/components/layout/AppHeader.tsx
@@ -4,11 +4,15 @@ import { ICON_LOGO } from "../../lib/constants"
 export default function AppHeader() {
 
     return (
-        <header>
-            <img src={ICON_LOGO} alt='Logo' />
-            <h1>Masquerade</h1>
+        <header className="absolute top-0 w-full h-[10vh] p-4 md:pr-10 flex flex-row justify-between items-center bg-primary">
+            <div className="h-full flex flex-row items-center space-x-2">
+                <img className="h-full aspect-square" src={ICON_LOGO} alt='Logo' />
+                <h1 className="text-xl md:text-3xl text-text font-jbm-bold">Masquerade</h1>
+            </div>
 
-            <Menu />
+            <button className="md:h-1/2 md:aspect-square">
+                <Menu className="w-full h-full text-text hover:text-accent hover:cursor-pointer" />
+            </button>
         </header>
     )
 }
