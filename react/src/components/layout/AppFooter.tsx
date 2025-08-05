df26e0a6cdd5e03edba6d23a2f2046451cf286c3
Robby Dharni <robbydharni@Robbys-MacBook-Pro.local>
2025-08-04 22:10:03 -0700

    app header layout complete

--- react/src/components/layout/AppFooter.tsx
+++ react/src/components/layout/AppFooter.tsx
@@ -3,7 +3,7 @@ export default function AppFooter() {
 
     return (
         <footer className="text-accent font-roboto fixed w-full h-[5vh] top-[95vh] flex flex-row justify-center items-center">
-            <p>Icons by <a href="https://icons8.com">Icons8</a></p>
+            <p>Icons by <a className="underline hover:text-text" href="https://icons8.com">Icons8</a></p>
         </footer>
     )
 }
