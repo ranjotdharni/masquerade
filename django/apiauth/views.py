369df54a8596346608bf833ff23289b032b0a797
Ranjot Dharni <ranjotdharni1@gmail.com>
2025-08-06 23:12:04 -0700

    database connected

--- django/apiauth/views.py
+++ django/apiauth/views.py
@@ -1,10 +1,26 @@
 from django.shortcuts import render
+from django.conf import settings
+
+import certifi
+
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
+from pymongo.mongo_client import MongoClient 
+from pymongo.server_api import ServerApi 
+
 # Create your views here.
+
+database_uri = settings.DB_CONNECTION_STRING
 
 class TestView(APIView):
     def get(self, request):
-        print("hit the api test endpoint")
+        client = MongoClient(database_uri, tlsCAFile=certifi.where())
+
+        try:
+            client.admin.command('ping')
+            print("Pinged your deployment. You successfully connected to MongoDB!")
+        except Exception as e:
+            print(e)
+
         return Response({ "message": "success" }, status=200)
