b6dc90b94db65bba9474fee4860ac6fbb0dcf1f0
Robby Dharni <robbydharni@Robbys-MacBook-Pro.local>
2025-08-07 21:27:22 -0700

    login page template complete

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,20 +1,26 @@
 import "./App.css"
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
-import { PAGE_HOME, PAGE_WELCOME } from "./lib/constants"
+import { PAGE_HOME, PAGE_LOGIN, PAGE_WELCOME } from "./lib/constants"
 import WelcomePage from "./pages/WelcomePage"
 import HomePage from "./pages/HomePage"
 import AppLayout from "./pages/AppLayout"
+import BaseLayout from "./pages/BaseLayout"
+import LoginPage from "./pages/LoginPage"
 
 const routes = createBrowserRouter([
   {
     path: PAGE_WELCOME,
-    element: <WelcomePage />
+    element: <BaseLayout />,
+    children: [
+      { index: true, element: <WelcomePage /> },
+      { path: PAGE_LOGIN, element: <LoginPage /> },
+    ]
   },
   {
     path: '/',
     element: <AppLayout />,
     children: [
-      { path: PAGE_HOME, element: <HomePage /> }
+      { path: PAGE_HOME, element: <HomePage /> },
     ],
   },
 ])
--- react/src/components/layout/AppFooter.tsx
+++ react/src/components/layout/AppFooter.tsx
@@ -2,7 +2,7 @@
 export default function AppFooter() {
 
     return (
-        <footer className="text-accent font-jbm fixed w-full h-[5vh] top-[95vh] flex flex-row justify-center items-center">
+        <footer className="text-accent font-roboto fixed w-full h-[5vh] top-[95vh] flex flex-row justify-center items-center">
             <p>Icons by <a className="underline hover:text-text" href="https://icons8.com">Icons8</a></p>
         </footer>
     )
