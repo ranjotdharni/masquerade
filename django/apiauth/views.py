610b22d26c42fc881a56f402e35936c42e6d37dc
Ranjot Dharni <ranjotdharni1@gmail.com>
2025-08-07 18:29:40 -0700

    user db connected

--- django/apiauth/views.py
+++ django/apiauth/views.py
@@ -1,20 +1,14 @@
 from django.shortcuts import render
 from django.conf import settings
 
-import certifi
-
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
-from pymongo.mongo_client import MongoClient
-
 # Create your views here.
-
-database_uri = settings.DB_CONNECTION_STRING
 
 class TestView(APIView):
     def get(self, request):
-        client = MongoClient(database_uri, tlsCAFile=certifi.where())
+        client = settings.MONGO_CLIENT
 
         try:
             client.admin.command('ping')
