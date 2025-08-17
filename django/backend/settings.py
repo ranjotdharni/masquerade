3b6ce47f159195f69b07a3557d49e802a52fa4f3
Ranjot Dharni <ranjotdharni1@gmail.com>
2025-08-17 16:17:04 -0700

    ready to instate auth checking on requests

--- django/backend/settings.py
+++ django/backend/settings.py
@@ -122,6 +122,9 @@ CORS_ALLOWED_ORIGINS = [
 ]
 
 CORS_ALLOW_CREDENTIALS = True
+SESSION_COOKIE_DOMAIN = None
+SESSION_COOKIE_SECURE = True
+SESSION_COOKIE_SAMESITE = "None"
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
