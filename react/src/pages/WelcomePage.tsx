a85c68d8eecd4f1d8799d10988c5ea7dbc18a435
Robby Dharni <robbydharni@Robbys-MacBook-Pro.local>
2025-08-21 03:31:21 -0700

    token refresh instated on client

--- react/src/pages/WelcomePage.tsx
+++ react/src/pages/WelcomePage.tsx
@@ -1,29 +1,40 @@
-import viteLogo from "../../public/vite.svg"
-import reactLogo from "../assets/react.svg"
+import { useState, type MouseEvent } from "react"
+import { PAGE_LOGIN, RESERVED_AUTH_STATUSES } from "../lib/constants"
+import { getCookie } from "../lib/utility/internal"
 
 export default function WelcomePage() {
+    const [message, setMessage] = useState<string>("")
+
+    async function testAPI(event: MouseEvent<HTMLButtonElement>) {
+        event.preventDefault()
+
+        fetch("https://127.0.0.1:8000/api/auth/signout/", {
+            method: "DELETE",
+            credentials: "include",
+            headers: {
+                "Content-Type": "application/json",
+                "X-CSRFToken": getCookie("csrftoken") as string,
+                "Authorization": `Bearer thisandthat`
+            }
+        }).then(middle => {
+            const reservedAuthStatusIndex: number = RESERVED_AUTH_STATUSES.findIndex(item => item.status === middle.status)
+
+            if (reservedAuthStatusIndex !== -1) {
+                setMessage(RESERVED_AUTH_STATUSES[reservedAuthStatusIndex].message)
+                window.location.href = "/login"
+                return
+            }
+
+            setMessage("")
+        })
+    }
+
     return (
-        <>
-            <div>
-                <a href="https://vite.dev" target="_blank">
-                <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://react.dev" target="_blank">
-                <img src={reactLogo} className="logo react" alt="React logo" />
-                </a>
-            </div>
-            <h1>Vite + React</h1>
-            <div className="card">
-                <button>
-                    Do Nothing
-                </button>
-                <p>
-                Edit <code>src/App.tsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className="read-the-docs">
-                Click on the Vite and React logos to learn more
-            </p>
-        </>
+        <section className="space-y-4 w-full h-[85vh] absolute left-0 top-[10vh] flex flex-col justify-center items-center">
+            <h1 className="text-xl font-lato-bold">Welcome to Masquerade!</h1>
+            <a href={PAGE_LOGIN} className="font-jbm text-sm appButton">Sign In</a>
+            <button onClick={testAPI} className="font-jbm text-sm appButton">Test API</button>
+            <p className="text-text font-jbm">{message}</p>
+        </section>
     )
 }
