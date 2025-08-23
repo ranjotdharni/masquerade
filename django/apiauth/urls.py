c659add35b61a0ce2a25fac28fe20cd8fe97d099
Robby Dharni <robbydharni@Robbys-MacBook-Pro.local>
2025-08-22 23:43:08 -0700

    basic sign up endpoint complete

--- django/apiauth/urls.py
+++ django/apiauth/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import GoogleSignIn, GithubSignIn, SignOut, RefreshTokens, ConfirmAuth
+from .views import GoogleSignIn, GithubSignIn, BasicSignUp, SignOut, RefreshTokens, ConfirmAuth
 
 urlpatterns = [
     path('google/init/', GoogleSignIn.as_view(), name="google_init"),
     path('github/init/', GithubSignIn.as_view(), name="github_init"),
+    path('basic/signup/', BasicSignUp.as_view(), name="basic_signup"),
     path('signout/', SignOut.as_view(), name="sign_out"),
     path('refresh/', RefreshTokens.as_view(), name="refresh_tokens"),
     path('confirm/', ConfirmAuth.as_view(), name="confirm_auth"),
