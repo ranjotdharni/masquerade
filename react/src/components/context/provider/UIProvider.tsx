922fd4feaf8500e8a1584c4e16c7a5b32fc015d7
Ranjot Dharni <ranjotdharni1@gmail.com>
2025-08-27 02:13:22 -0700

    ui notify + confirm modals complete

--- react/src/components/context/provider/UIProvider.tsx
+++ react/src/components/context/provider/UIProvider.tsx
@@ -6,10 +6,7 @@ import useTimeout from "../../../lib/hooks/useTimeout"
 const UI_NOTIFICATION_MILLISECONDS: number = 8000
 
 function Notification({ message, color, reset } : NotificationProps & { reset: () => void }) {
-    let hiddenTopValue: string = "103vh"
-    let visibleTopValue: string = "90vh"
-
-    const [top, setTop] = useState<string>(hiddenTopValue)
+    const [translateY, setTranslateY] = useState<string>("translateY(100%)")
     const hideTextTimer = useTimeout(0)
     const clearTextTimer = useTimeout(0)
 
@@ -17,11 +14,11 @@ function Notification({ message, color, reset } : NotificationProps & { reset: (
         if (message.trim() !== "" && !hideTextTimer.completed) {
             hideTextTimer.begin(UI_NOTIFICATION_MILLISECONDS)
             clearTextTimer.begin(UI_NOTIFICATION_MILLISECONDS + 1000)
-            setTop(visibleTopValue)
+            setTranslateY("translateY(0%)")
         }
         
         if (hideTextTimer.completed) {
-            setTop(hiddenTopValue)
+            setTranslateY("translateY(100%)")
         }
 
         if (clearTextTimer.completed) {
@@ -32,9 +29,11 @@ function Notification({ message, color, reset } : NotificationProps & { reset: (
     }, [message, hideTextTimer.completed, clearTextTimer.completed])
 
     return (
-        <span style={{color: color, top: top, transition: "top 0.3s ease"}} className="absolute w-[90%] h-auto left-[5%] py-4 md:left-[2.5%] md:w-auto md:px-8 font-jbm flex flex-row justify-center items-center border bg-background border-accent rounded-lg">
-            <p>{message}</p>
-        </span>
+        <div style={{transform: translateY, transition: "transform 0.4s ease"}} className="absolute w-[90%] h-[20vh] left-[5%] top-[90vh] flex flex-col justify-start items-center md:items-end">
+            <span style={{color: color}} className="w-full h-auto max-h-1/2 py-4 md:w-auto md:px-8 font-jbm flex flex-row justify-center items-center border bg-background border-accent rounded-lg">
+                <p>{message}</p>
+            </span>
+        </div>
     )
 }
 
@@ -52,16 +51,18 @@ function Confirm({ message, callback, reset } : ConfirmProps & { reset: () => vo
     }
 
     return (
-        <aside>
-            <h3>Are You Sure?</h3>
-            <span>
-                <p>{message}</p>
-            </span>
-            <div>
-                <button onClick={onCancel}>Cancel</button>
-                <button onClick={onConfirm}>Confirm</button>
-            </div>
-        </aside>
+        <div className="z-40 absolute w-[100vw] h-[100vh] flex flex-col justify-center items-center popInBlur">
+            <aside className="relative w-[90%] h-[50%] md:w-1/5 md:aspect-video md:h-auto p-2 flex flex-col justify-start items-center border bg-background border-accent popIn">
+                <h3 className="w-full px-2 border-b border-accent text-xl text-error font-jbm-bold">Are You Sure?</h3>
+                <span className="w-full p-2 flex-1 text-text font-jbm">
+                    <p>{message}</p>
+                </span>
+                <div className="w-full h-auto flex flex-row justify-end space-x-2">
+                    <button onClick={onCancel} className="appButton bg-error! text-background!">Cancel</button>
+                    <button onClick={onConfirm} className="appButton bg-text! text-background!">Confirm</button>
+                </div>
+            </aside>
+        </div>
     )
 }
 
