35e58da89b94c2afe986dbc1727cd6ceaf69bfdb
Ranjot Dharni <ranjotdharni1@gmail.com>
2025-09-17 20:23:51 -0700

    full validation for survey creation (simplify database update)

--- django/survey/utils.py
+++ django/survey/utils.py
@@ -40,21 +40,21 @@ def validate_survey_creation_slug(slug):
     try:
         validate(instance=slug, schema=survey_creation_schema)
 
-        if len(slug["name"].strip()) == 0:
-            return GenericError(message="Please provide a survey name.").to_dict()
+        if len(slug["name"].strip()) < 1 or len(slug["name"].strip()) > 128:
+            return GenericError(message="Please provide a survey name (max length: 128).").to_dict()
         
         if len(slug["questions"]) == 0 or len(slug["questions"]) > settings.MAX_QUESTIONS_PER_SURVEY:
             return GenericError(message="Survey must include 1 to 10 questions.").to_dict()
         
         for question in slug["questions"]:
-            if len(question["question"].strip()) == 0:
-                return GenericError(message="All questions must have a value.").to_dict()
+            if len(question["question"].strip()) < 1 or len(question["question"].strip()) > 256:
+                return GenericError(message="All questions' text must have a value (max length: 256).").to_dict()
             
             if question["type"] not in settings.QUESTION_TYPE_IDS:
                 return GenericError(message="An unrecognized question type was detected.").to_dict()
             
-            if question["type"] != settings.RATING_ID and ("answers" not in question or len(question["answers"]) == 0 or len(question["answers"]) > settings.MAX_ANSWERS_PER_QUESTION or len(question["answers"][0]["answer"].strip()) == 0):
-                return GenericError(message="Non-rating type questions must include 1 to 4 answers.").to_dict()
+            if question["type"] != settings.RATING_ID and ("answers" not in question or len(question["answers"]) == 0 or len(question["answers"]) > settings.MAX_ANSWERS_PER_QUESTION or any(len(a["answer"].strip()) < 1 or len(a["answer"].strip()) > 256 for a in question["answers"])):
+                return GenericError(message="Non-rating type questions must include 1 to 4 answers each with a max length of 256 characters.").to_dict()
         
         return { "success": True, "message": "Survey parameters are valid." }
     except Exception as e:
