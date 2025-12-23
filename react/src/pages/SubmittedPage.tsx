d5868cb6edb7c816a3a76a84d97a2423976a9ccd
Ranjot Dharni <ranjotdharni1@gmail.com>
2025-12-22 20:19:10 -0800

    successful submission page created

---
+++ react/src/pages/SubmittedPage.tsx
@@ -0,0 +1,10 @@
+import AppContent from "../components/layout/AppContent";
+
+export default function SubmittedPage() {
+
+    return (
+        <AppContent>
+            <h1>Survey submitted successfully!</h1>
+        </AppContent>
+    )
+}
