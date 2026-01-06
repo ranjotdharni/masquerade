4cc2b40f8d9b36e8d556d797894917527d893161
Ranjot Dharni <ranjotdharni1@gmail.com>
2026-01-06 02:11:16 -0800

    my surveys page ui complete (not responsive)

--- react/src/components/MySurveysPage/MySurveyHeader.tsx
+++ react/src/components/MySurveysPage/MySurveyHeader.tsx
@@ -6,7 +6,7 @@ export default function MySurveyHeader({ username, numberOfSurveys } : { usernam
             <div className="w-full h-full p-2 bg-accent rounded-lg">
                 <h1 className="h-3/5 text-[2rem] text-text font-jbm-bold">Your Surveys</h1>
                 <div className="w-full h-2/5 px-4 flex flex-col justify-center items-end">
-                    <h2 className="text-[1.5rem] text-secondary">{numberOfSurveys}</h2>
+                    <p className="text-[1.5rem] text-text flex flex-row items-center after:content-['Surveys'] after:text-sm after:ml-1">{numberOfSurveys}</p>
                     <p className="font-lato-italic text-inactive">{username}</p>
                 </div>
             </div>
--- react/src/components/MySurveysPage/MySurveyList.tsx
+++ react/src/components/MySurveysPage/MySurveyList.tsx
@@ -1,10 +1,31 @@
+import { LockOpen, Lock, ChevronRight, Plus } from "lucide-react"
 import type { SurveyMetadata } from "../../lib/types/api"
+import { PAGE_SURVEY_CREATE, PAGE_SURVEY_TAKE, PAGE_SURVEY_VIEW } from "../../lib/constants"
 
 function SurveyWidget({ survey } : { survey: SurveyMetadata }) {
 
     return (
-        <li>
-           <h3>{survey.name}</h3> 
+        <li className="w-full h-30 px-2 pt-1 pb-3 mt-2 flex flex-col justify-between rounded border border-accent">
+            <div className="w-full flex flex-row justify-between">
+                <h3 className="font-lato-italic text-[1.5rem] text-primary">{survey.name}</h3> 
+                <p className="text-text flex flex-row items-center after:content-['Questions'] after:text-xs after:ml-1">{survey.numberOfQuestions}</p>
+            </div>
+
+            <div className="pl-1 flex flex-row justify-between">
+                {
+                    survey.inviteOnly ? 
+                    <Lock className="text-error" strokeWidth={2.5} /> :
+                    <LockOpen className="text-inactive" opacity={0.25} strokeWidth={2.5} />
+                }
+                
+                <div className="flex flex-row">
+                    <a href={`/${PAGE_SURVEY_VIEW}/${survey._id.$oid}`} className="flex flex-row items-center text-inactive scale-75 hover:cursor-pointer hover:text-text">
+                        <p className="font-jbm-italic">View</p>
+                        <ChevronRight />
+                    </a>
+                    <a href={`/${PAGE_SURVEY_TAKE}/${survey._id.$oid}`} className="font-lato appButton">Participate</a>
+                </div>
+            </div>
         </li>
     )
 }
@@ -12,8 +33,10 @@ function SurveyWidget({ survey } : { survey: SurveyMetadata }) {
 export default function MySurveyList({ surveys } : { surveys: SurveyMetadata[] }) {
 
     return (
-        <ul className="border-t border-accent">
+        <ul style={surveys.length === 0 ? {display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center"} : undefined} className="h-full border-t border-text overflow-y-scroll overflow-x-hidden px-4 pb-4">
             {
+                surveys.length === 0 ?
+                <a href={`/${PAGE_SURVEY_CREATE}`} className="appButton font-jbm flex flex-row"><Plus className="mr-2" /> Create</a> : 
                 surveys.map(s => {
                     return <SurveyWidget key={s._id.$oid} survey={s} />
                 })
