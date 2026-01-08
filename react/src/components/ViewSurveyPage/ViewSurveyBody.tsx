1a2f6bca4022b6b9ca0c45b1129f1a49f7189f01
Ranjot Dharni <ranjotdharni1@gmail.com>
2026-01-07 19:18:38 -0800

    view survey page components created

---
+++ react/src/components/ViewSurveyPage/ViewSurveyBody.tsx
@@ -0,0 +1,7 @@
+
+export default function ViewSurveyBody() {
+
+    return (
+        <></>
+    )
+}
--- react/src/pages/ViewSurveyPage.tsx
+++ react/src/pages/ViewSurveyPage.tsx
@@ -2,18 +2,36 @@ import { useNavigate, useParams } from "react-router-dom"
 import AppContent from "../components/layout/AppContent"
 import { PAGE_SURVEY_FIND } from "../lib/constants"
 import FullScreenLoader from "../components/utility/FullScreenLoader"
+import type { Survey } from "../lib/types/api"
+import SampleData from "../sampledata/surveys.json"
+import { useState } from "react"
+import ViewSurveyHeader from "../components/ViewSurveyPage/ViewSurveyHeader"
+import ViewSurveyBody from "../components/ViewSurveyPage/ViewSurveyBody"
 
 export default function ViewSurveyPage() {
     const navigate = useNavigate()
     const { id } = useParams()
 
+    const [content, setContent] = useState<Survey>(SampleData.content as unknown as Survey)
+
     if (id === undefined)
         navigate(`/${PAGE_SURVEY_FIND}`)
+
+    function PageContent({ content } : { content: Survey }) {
+        return (
+            <>
+                <ViewSurveyHeader />
+                <ViewSurveyBody />
+            </>
+        )
+    }
 
     return (
         <AppContent className="flex flex-col justify-center items-center">
             {
-                <FullScreenLoader loaderText={`Searching for ID: ${id}`} width="25%" aspectRatio={"9 / 16"} />
+                content ?
+                <FullScreenLoader loaderText={`Searching for ID: ${id}`} width="25%" aspectRatio={"9 / 16"} />  : 
+                <PageContent content={content} />
             }
         </AppContent>
     )
