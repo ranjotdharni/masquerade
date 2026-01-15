99ef57c9bae13ee075f4f88fed70e987c880cb54
Ranjot Dharni <ranjotdharni1@gmail.com>
2026-01-15 03:32:18 -0800

    client api types reflecting database/api

--- react/src/lib/types/api.ts
+++ react/src/lib/types/api.ts
@@ -5,36 +5,71 @@ export type ObjectId = {
     $oid: string
 }
 
+export type SingleChoiceAnswer = {
+  _id: ObjectId
+  answer: string
+  submissions: number
+}
+
+export type MultipleChoiceAnswer = {
+  _id: ObjectId
+  answer: string
+  submissions: number
+}
+
+export type RankingAnswer = {
+  _id: ObjectId
+  answer: string
+  1: number
+  2: number
+  3: number
+  4: number
+}
+
+export type RatingAnswer = {
+  1: number
+  2: number
+  3: number
+  4: number
+  5: number
+}
+
+export type BaseAnswer = 
+  | SingleChoiceAnswer[]
+  | MultipleChoiceAnswer[]
+  | RankingAnswer[]
+  | RatingAnswer
+
 interface BaseQuestion {
-    _id: ObjectId
-    type: QuestionIdType
-    optional: number
-    question: string
+  _id: ObjectId
+  type: QuestionIdType
+  optional: boolean
+  submissions?: number
+  question: string
+  answers?: BaseAnswer
 }
 
 export interface SingleChoiceQuestion extends BaseQuestion {
-    type: typeof QUESTION_TYPE_ID_MAP.SINGLE_CHOICE_TYPE
-    answers: { answer: string, _id: ObjectId }[]
+  type: typeof QUESTION_TYPE_ID_MAP.SINGLE_CHOICE_TYPE
+  answers?: SingleChoiceAnswer[]
 }
 
 
 export interface MultipleChoiceQuestion extends BaseQuestion {
   type: typeof QUESTION_TYPE_ID_MAP.MULTIPLE_CHOICE_TYPE
-  answers: { answer: string, _id: ObjectId }[]
+  answers?: MultipleChoiceAnswer[]
 }
 
 
 export interface RankingQuestion extends BaseQuestion {
   type: typeof QUESTION_TYPE_ID_MAP.RANKING_TYPE
-  answers: { answer: string, _id: ObjectId }[]
+  answers?: RankingAnswer[]
 }
 
 
 export interface RatingQuestion extends BaseQuestion {
   type: typeof QUESTION_TYPE_ID_MAP.RATING_TYPE
-  answers: {
-    answer?: number
-  } | {}
+  answers?: RatingAnswer
 }
 
 export type Question =
@@ -45,8 +80,10 @@ export type Question =
 
 export interface Survey {
   _id: ObjectId
+  creator?: string
   name: string
   inviteOnly: boolean
+  submissions?: number
   questions: Question[]
 }
 
