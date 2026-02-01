aa288775f65ac13b4c9163cf1b9a239d6ef374bc
Ranjot Dharni <ranjotdharni1@gmail.com>
2026-01-31 22:15:08 -0800

    landing page iconic info section complete (responsive + tested)

--- react/src/components/WelcomePage/sections/IconicInfoSection.tsx
+++ react/src/components/WelcomePage/sections/IconicInfoSection.tsx
@@ -1,4 +1,4 @@
-import { ICON_ANONYMOUS, ICON_APEXCHARTS, ICON_DATA, ICON_DJANGO, ICON_GROUP, ICON_MONGO, ICON_OAUTH, ICON_REACT, ICON_UI } from "../../../lib/constants";
+import { ICON_ANONYMOUS, ICON_APEXCHARTS, ICON_DATA, ICON_DJANGO, ICON_GROUP, ICON_MONGO, ICON_OAUTH, ICON_REACT, ICON_UI } from "../../../lib/constants"
 
 export default function IconicInfoSection() {
     const srcArray = [
@@ -51,22 +51,22 @@ export default function IconicInfoSection() {
         <section className="w-full flex flex-col items-center mt-16">
             <h3 className="text-2xl font-roboto-bold text-secondary">Built with trusted tools</h3>
 
-            <article className="flex flex-row space-x-10 mt-6">
+            <article className="flex flex-row justify-center space-x-10 mt-6">
                 {
                     srcArray.map((detail, index) => {
-                        return <img key={`ICONIC_INFO_TOOL_1x${index}`} src={detail.src} alt={detail.alt} className="h-16 aspect-square" />
+                        return <img key={`ICONIC_INFO_TOOL_1x${index}`} src={detail.src} alt={detail.alt} className="md:h-16 md:w-auto w-[10%] aspect-square" />
                     })
                 }
             </article>
 
-            <article className="flex flex-row items-center justify-center space-x-14 mt-18">
+            <article className="flex flex-col md:flex-row items-center md:justify-center md:space-y-0 md:space-x-14 mt-18 space-y-10">
                 {
                     iconDetailsArray.map((detail, index) => {
                         return (
                             <figure key={`ICONIC_INFO_DETAIL_1x${index}`} className="flex flex-col justify-end items-center space-y-4">
-                                <img src={detail.src} className="w-24 aspect-square bg-text p-4 rounded-[100px]" />
+                                <img src={detail.src} className="md:w-24 aspect-square bg-text p-4 rounded-[100px]" />
                                 <h4 className="text-xl text-secondary font-jbm-bold">{detail.title}</h4>
-                                <figcaption className="w-40 text-center text-center text-inactive font-roboto">{detail.text}</figcaption>
+                                <figcaption className="w-40 text-center text-center text-inactive font-roboto-italic">{detail.text}</figcaption>
                             </figure>
                         )
                     })
