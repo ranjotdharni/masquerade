458a880dea679067e9969e0e03724bc62b9b47d9
Ranjot Dharni <ranjotdharni1@gmail.com>
2026-02-15 18:30:26 -0800

    google state parameter on login added

--- react/src/components/LoginPage/SignInForm.tsx
+++ react/src/components/LoginPage/SignInForm.tsx
@@ -7,11 +7,13 @@ const PROVIDER_KEY_NAME: string = "provider"
 
 const GOOGLE_CODE_NAME: string = "code"
 const GOOGLE_SCOPE_NAME: string = "scope"
+const GOOGLE_STATE_NAME: string = "state"
 const GOOGLE_ERROR_NAME: string = "error"
 
 type GoogleCredentials = {
     [GOOGLE_CODE_NAME]: string
     [GOOGLE_SCOPE_NAME]: string
+    [GOOGLE_STATE_NAME]: string
 }
 
 type SignInFormProps = {
@@ -82,6 +84,7 @@ export default function SignInForm({ setError, setLoader } : SignInFormProps) {
             redirect_uri: import.meta.env.VITE_GOOGLE_REDIRECT_URI,
             response_type: "code",
             scope: "openid email profile",
+            state: state
         })
 
         window.location.href = `https://accounts.google.com/o/oauth2/v2/auth?${params}`
@@ -90,7 +93,7 @@ export default function SignInForm({ setError, setLoader } : SignInFormProps) {
     }
 
     function initGoogleTokenExchange(): GoogleCredentials | false {
-        const stateExists = sessionStorage.getItem(STATE_KEY_NAME) !== null
+        const storedState = sessionStorage.getItem(STATE_KEY_NAME)
 
         const rawParamString = location.search
 
@@ -109,11 +112,13 @@ export default function SignInForm({ setError, setLoader } : SignInFormProps) {
 
         const code = params.get(GOOGLE_CODE_NAME)
         const scope = params.get(GOOGLE_SCOPE_NAME)
+        const state = params.get(GOOGLE_STATE_NAME)
 
         const readyForTokenExchange = (
-            stateExists && 
             code !== null && 
-            scope !== null 
+            scope !== null && 
+            storedState !== null && 
+            storedState === state
         )
 
         if (readyForTokenExchange) {
@@ -189,16 +194,16 @@ export default function SignInForm({ setError, setLoader } : SignInFormProps) {
     }, [])
 
     return (
-        <div className="mt-12 flex flex-col items-center">
+        <section className="mt-12 flex flex-col items-center">
             <h1 className="text-2xl xl:text-3xl font-lato-bold text-text">
                 Sign In
             </h1>
-            <div className="w-full flex-1 mt-8">
+            <article className="w-full flex-1 mt-8">
                 <div className="flex flex-col items-center">
                     <button
                         onClick={signInWithGoogle}
                         className="w-full max-w-xs font-jbm shadow-sm rounded-lg py-3 bg-accent text-text hover:text-primary flex items-center justify-center transition-all duration-300 ease-in-out focus:outline-none hover:cursor-pointer hover:shadow focus:shadow-sm focus:shadow-outline">
-                        <div className="bg-white p-2 rounded-full">
+                        <figure className="bg-white p-2 rounded-full">
                             <svg className="w-4" viewBox="0 0 533.5 544.3">
                                 <path
                                     d="M533.5 278.4c0-18.5-1.5-37.1-4.7-55.3H272.1v104.8h147c-6.1 33.8-25.7 63.7-54.4 82.7v68h87.7c51.5-47.4 81.1-117.4 81.1-200.2z"
@@ -213,7 +218,7 @@ export default function SignInForm({ setError, setLoader } : SignInFormProps) {
                                     d="M272.1 107.7c38.8-.6 76.3 14 104.4 40.8l77.7-77.7C405 24.6 339.7-.8 272.1 0 169.2 0 75.1 58 28.9 150l90.4 70.1c21.5-64.5 81.8-112.4 152.8-112.4z"
                                     fill="#ea4335" />
                             </svg>
-                        </div>
+                        </figure>
                         <span className="ml-4">
                             Sign In with Google
                         </span>
@@ -222,12 +227,12 @@ export default function SignInForm({ setError, setLoader } : SignInFormProps) {
                     <button
                         onClick={signInWithGithub}
                         className="w-full max-w-xs font-jbm shadow-sm rounded-lg py-3 bg-accent text-text hover:text-primary flex items-center justify-center transition-all duration-300 ease-in-out focus:outline-none hover:cursor-pointer hover:shadow focus:shadow-sm focus:shadow-outline mt-5">
-                        <div className="bg-white p-1 rounded-full">
+                        <figure className="bg-white p-1 rounded-full">
                             <svg className="w-6" viewBox="0 0 32 32">
                                 <path fill-rule="evenodd"
                                     d="M16 4C9.371 4 4 9.371 4 16c0 5.3 3.438 9.8 8.207 11.387.602.11.82-.258.82-.578 0-.286-.011-1.04-.015-2.04-3.34.723-4.043-1.609-4.043-1.609-.547-1.387-1.332-1.758-1.332-1.758-1.09-.742.082-.726.082-.726 1.203.086 1.836 1.234 1.836 1.234 1.07 1.836 2.808 1.305 3.492 1 .11-.777.422-1.305.762-1.605-2.664-.301-5.465-1.332-5.465-5.93 0-1.313.469-2.383 1.234-3.223-.121-.3-.535-1.523.117-3.175 0 0 1.008-.32 3.301 1.23A11.487 11.487 0 0116 9.805c1.02.004 2.047.136 3.004.402 2.293-1.55 3.297-1.23 3.297-1.23.656 1.652.246 2.875.12 3.175.77.84 1.231 1.91 1.231 3.223 0 4.61-2.804 5.621-5.476 5.922.43.367.812 1.101.812 2.219 0 1.605-.011 2.898-.011 3.293 0 .32.214.695.824.578C24.566 25.797 28 21.3 28 16c0-6.629-5.371-12-12-12z" />
                             </svg>
-                        </div>
+                        </figure>
                         <span className="ml-4">
                             Sign In with GitHub
                         </span>
@@ -235,10 +240,10 @@ export default function SignInForm({ setError, setLoader } : SignInFormProps) {
                 </div>
 
                 <div className="my-12 border-b text-center border-primary dark:border-text">
-                    <div
+                    <span
                         className="leading-none px-2 inline-block text-sm text-accent tracking-wide font-medium bg-white dark:bg-primary transform translate-y-1/2">
                         Or
-                    </div>
+                    </span>
                 </div>
 
                 <form onSubmit={basicSignIn} className="mx-auto max-w-xs">
@@ -261,7 +266,7 @@ export default function SignInForm({ setError, setLoader } : SignInFormProps) {
                         </span>
                     </button>
                 </form>
-            </div>
-        </div>
+            </article>
+        </section>
     )
 }
