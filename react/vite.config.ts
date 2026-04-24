8f4be9ed1638d77dcb5023a92da6e8428cb0090d
Ranjot Dharni <ranjotdharni1@gmail.com>
2026-04-23 19:09:27 -0700

    update ts and vite config for @ paths

--- react/vite.config.ts
+++ react/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import basicSsl from '@vitejs/plugin-basic-ssl'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,4 +11,9 @@ export default defineConfig({
     tailwindcss(),
     basicSsl(),
   ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
